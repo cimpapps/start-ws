e36ef449fa89e1fc69f17d34c7a055cf82c8ed9a
catalincimpoeru <catalincimpoeru24@gmail.com>
2020-06-08 14:52:16 +0300

    WEBSOCKETS WITHOUT STOMP

---
+++ src/test/java/com/thejavacademy/startws/StartWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thejavacademy.startws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartWsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
